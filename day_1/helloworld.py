35627a47def849de0d49fac0636b1d66ab8018f3
liangxiaole <xiaoleliang045@gmail.com>
2022-08-09 21:40:10 +0900

    finish 1 and 2 tasks

---
+++ day_1/helloworld.py
@@ -0,0 +1,22 @@
+# 1. Check the python version you are using
+print('Python 3.10.6')
+
+# 2. Open the python interactive shell and do the following operations. The operands are 3 and 4. Check the example above
+'''
+addition(+)
+subtraction(-)
+multiplication(*)
+modulus(%)
+division(/)
+exponential(**)
+floor division operator(//)
+'''
+print(2 + 3)
+print(2 - 2)
+print(2 * 2)
+print(2 % 2)
+print(2 / 2)
+print(2 ** 2)
+print(2 // 2)
+
+# 3. 
