82172346b2ee464da0856859386efd9b53f3b218
liangxiaole <xiaoleliang045@gmail.com>
2022-08-09 21:43:37 +0900

    finish task 3 and 4

--- day_1/helloworld.py
+++ day_1/helloworld.py
@@ -19,4 +19,32 @@ print(2 / 2)
 print(2 ** 2)
 print(2 // 2)
 
-# 3. 
+# 3. Write strings on the python interactive shell. The strings are the following:
+'''
+Your name
+Your family name
+Your country
+I am enjoying 30 days of python
+'''
+print('Young Driver Of Tech')
+print('Young Driver Of Tech')
+print('Japan')
+print('I am enjoing 30 days of python')
+
+# 4. Check the data types of the following data
+'''
+10
+9.8
+3.14
+4 - 4j
+['Asabeneh', 'Python', 'Finland']; it is a list if data is wrapped by []. It's tuple if data is wrapper by {}
+Your name
+Your family name
+Your country
+'''
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('wahaha'))
